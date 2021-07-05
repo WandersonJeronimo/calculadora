c47872729ebcd1fc4e65b2c18494c112d431fea7
Wanderson <wandersonjeronimo@gmail.com>
2021-07-05 13:49:38 -0300

    Primeiro commit

---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' Função para subtrair
+#'
+#' Esta função serve para subtrair dois números
+#'
+#' @param a Um número
+#' @param b Outro número
+#'
+#' @examples
+#' somar(3,4)
+#'
+#' @export
+subtrair <- function(a, b){
+  a - b
+}
